2630e0e28e9224016931a6132221dd6082e1e04a
zhaofeng-shu33 <616545598@qq.com>
2019-09-01 20:18:58 +0800

    fix travis copy

--- local_rar_test.sh
+++ local_rar_test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 export LOCALRAR=1
+if [[ -z "$TRAVIS" ]]; then
+  rm -r rar
+fi
 cp /usr/local/bin/rar ./
 php test.php
 ./integration_test.sh
 # do not remove the rar on travis ci
-if [[ ! -v $TRAVIS ]]; then
+if [[ ! -z "$TRAVIS" ]]; then
   rm rar
 fi
 unset LOCALRAR
